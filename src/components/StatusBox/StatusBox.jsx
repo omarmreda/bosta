36cba0c3fbec8a70e1968698e73ba9949686fc6d
Omar <omarmreda95@gmail.com>
2023-11-07 22:07:51 +0200

    navbar update

---
+++ src/components/StatusBox/StatusBox.jsx
@@ -0,0 +1,26 @@
+
+
+const StatusBox = () => {
+    return (
+        <section className="mt-20 border border-r-3 max-w-6xl  mx-auto px-8">
+            <table>
+                <tr className="flex justify-between">
+                    <th>Shipment Number 6741696</th>
+                    <th>Latest update</th>
+                    <th>Store Name</th>
+                    <th>Reciving Date</th>
+                </tr>
+                <tr className="flex justify-between">
+                    <td>Shipment is not delivered</td>
+                    <td>Monday 06/4/2020 5:33 pm</td>
+                    <td>SOUQ.COM</td>
+                    <td>3 Jan 2020</td>
+                </tr>
+            </table>
+            <hr />
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum ut, ex et libero aspernatur quod accusamus natus temporibus voluptatibus, quisquam dolor facilis, vitae amet? Omnis consectetur quibusdam suscipit nulla quia.
+        </section>
+    )
+}
+
+export default StatusBox
